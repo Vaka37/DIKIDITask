584ff823afee68ec5e046a54d94fc069081b5241
Vaka37 <vak-forever@inbox.ru>
2024-06-19 22:28:15 +0300

    app init

---
+++ DIKIDI/DIKIDI/DIKIDIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIKIDIApp.swift
+//  DIKIDI
+//
+//  Created by Vakil on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DIKIDIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
